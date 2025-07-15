c0eb8b98d4fa7054bf5ae013b6f2009a6b84e04b
LiuHarry1 <593954214@qq.com>
2025-07-15 18:32:33 +0800

    leecode

---
+++ study_python/leecode/backtrack/subsets.py
@@ -0,0 +1,24 @@
+
+"""
+78. 子集
+输入：nums = [1,2,3]
+输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+"""
+def subsets(nums):
+    res = []
+
+    def backtrack(start, path):
+        # res.append(path.copy())  # 添加当前路径到结果
+        res.append(path[:])
+        for i in range(start, len(nums)):
+            path.append(nums[i])  # 选择当前数字
+            backtrack(i + 1, path)  # 递归下一层
+            path.pop()  # 撤销选择（回溯）
+
+    backtrack(0, [])
+    return res
+
+if __name__ == '__main__':
+    result = subsets([1,2,3])
+    print(result)
