49191989ebc36ac63d8f37decee712f9e41e63ed
茫茫人海一粒沙 <593954214@qq.com>
2025-07-17 23:05:51 +0800

    studying

--- study_python/leecode/dp/numSquares.py
+++ study_python/leecode/dp/numSquares.py
@@ -4,23 +4,16 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        # 先检查是否本身就是完全平方数
-        if int(n ** 0.5) ** 2 == n:
-            return 1
+        dp = [float('inf')] *(n+1)
+        dp[0] =0
+        for i in range(1, n+1):
+            min_val = i
+            j=1
+            while j*j <=i:
+                min_val = min(min_val, dp[i-j*j] +1)
+                j +=1
 
-        dp = [float('inf')] * (n + 1)
-        dp[0] = 0  # 基础情况
-
-        max_square = int(n ** 0.5) + 1
-        square_nums = [i * i for i in range(1, max_square)]
-
-        for i in range(1, n + 1):
-            for square in square_nums:
-                if square > i:
-                    break
-                dp[i] = min(dp[i], dp[i - square] + 1)
-
-        return dp[n]
+            dp[i] = min_val
 
 if __name__ == '__main__':
     sol = Solution()
